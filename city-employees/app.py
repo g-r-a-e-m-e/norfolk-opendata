b580763d4a11014936e4f7074e7e9b152d4a1519
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2023-05-02 16:51:56 -0400

    create app.py

---
+++ city-employees/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+app.py
+
+This is the Streamlit app to explore and analyze City of Norfolk OpenData
+
+Created on Tue May  2 16:40:13 2023
+
+@author: https://github.com/g-r-a-e-m-e
+"""
+
+# Import packages
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
