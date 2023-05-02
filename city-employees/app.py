d7b3b14e7ec19c498548d605fdc7ba9dbec35ac9
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2023-05-02 17:39:14 -0400

    update app.py

--- city-employees/app.py
+++ city-employees/app.py
@@ -16,10 +16,25 @@ import plotly.express as px
 import streamlit as st
 from pipeline import pull_data
 
-## Helper functions
-#def refresh_data(refresh = False):
-#    if refresh == True:
-#        pull_data()
+##### Helper functions #####
+
+# refresh_data will pull current data available from https://data.norfolk.gov
+def refresh_data(refresh = False):
+    if refresh == True:
+        pull_data()
+
+##### Create Pandas DataFrames from data in /data #####
+census_df = pd.read_csv('data/census.csv')
+demographics_df = pd.read_csv('data/demographics.csv')
+salaries_df = pd.read_csv('data/salaries.csv') 
+
+##### Create Plotly figures #####
+salaries_by_department_violin = px.violin(data_frame = salaries_df,
+                                          x = 'department',
+                                          y = 'annual_base_rate',
+                                          color = 'division')
 
 ##### Streamlit configuration #####
 st.title('City of Norfolk Employee Data')
+
+st.plotly_chart(salaries_by_department_violin)
