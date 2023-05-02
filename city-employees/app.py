b184beadac379053d86aca0f69d3fae8a691feff
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2023-05-02 17:11:51 -0400

    update pipelines.py and app.py

--- city-employees/app.py
+++ city-employees/app.py
@@ -14,4 +14,12 @@ Created on Tue May  2 16:40:13 2023
 import pandas as pd
 import plotly.express as px
 import streamlit as st
+from pipeline import pull_data
 
+## Helper functions
+#def refresh_data(refresh = False):
+#    if refresh == True:
+#        pull_data()
+
+##### Streamlit configuration #####
+st.title('City of Norfolk Employee Data')
--- city-employees/pipeline.py
+++ city-employees/pipeline.py
@@ -23,21 +23,18 @@ salaries = 'https://data.norfolk.gov/resource/4fsk-z8s8.json'
 demographics = 'https://data.norfolk.gov/resource/vv96-9m5c.json'
 census = 'https://data.norfolk.gov/resource/dijs-dhze.json'
 
-# Function to pull current current data
-def pull_data(json_path, filename: str):
-    # Read in json data to Pandas DataFrame
-    df = pd.read_json(json_path)
-    # Write out dataframe to data directory
-    return df.to_csv(f'data/{filename}')
-
 # Specify endpoints 
 endpoints = {'salaries': salaries,
              'demographics': demographics,
              'census': census}
 
-# Loop over endpoints and pull current data, stage to current 
-for idx, nm in enumerate(endpoints):
-    print(f'\nPulling {nm} data...\n')
-    pull_data(endpoints[nm], f'{nm}.csv')
-
-print('Done!')
+# Function to pull current current data
+def pull_data(endpoints = endpoints):
+    # Loop over endpoints and pull data
+    for idx, nm in enumerate(endpoints):
+        print(f'\nPulling {nm} data...\n')
+        # Read in json data to Pandas DataFrame
+        df = pd.read_json(endpoints[nm])
+        # Write out dataframe to data directory
+        df.to_csv(f'data/{nm}.csv')
+    return print('\nDone!\n') 
