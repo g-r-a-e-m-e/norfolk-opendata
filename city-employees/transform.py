d7c447effc70f4ad76ea306f1dadb91016c3dd02
graeme benson <62527977+g-r-a-e-m-e@users.noreply.github.com>
2023-05-03 11:43:47 -0400

    update transform.py

--- city-employees/transform.py
+++ city-employees/transform.py
@@ -12,8 +12,12 @@ Created on Wed May  3 10:17:05 2023
 """
 
 # Import packages
+import datetime as dt
 import pandas as pd
-import numpy as np
+from pipeline import pull_data
+
+# Pull current data
+pull_data()
 
 # Read in data
 census_df = pd.read_csv('data/census.csv')
@@ -27,4 +31,71 @@ def census_transform(data = census_df):
     data['decadal_delta_2020_vs_2010'] = data['census_2020'] - data['census_2010']
     data['decadal_delta_2000_vs_2000'] = data['census_2010'] - data['census_2000']
     
-    return data.to_csv('data/census.csv')
+    # Print status
+    print('\nCreating additional census features...\n')
+    
+    # Write to /data
+    data.to_csv('data/census.csv')
+    
+    return  print('\nDone!\n')
+
+
+def demographics_transform(data = demographics_df):
+    # Create current_year variable to create additional fields
+    current_year = int(dt.datetime.now().strftime('%Y'))
+    
+    # Create tenure field to specify length of time with City of Norfolk
+    data['tenure'] = data['startyear'].apply(lambda x: current_year - x)
+    
+    # Create age field to determine employee age
+    data['age'] = data['birthyear'].apply(lambda x: current_year - x)
+    
+    # Print status
+    print('\nCreating additional demographics features...\n')
+    
+    # Write to /data
+    data.to_csv('data/demographics.csv')
+    
+    return print('\nDone!\n')
+
+def salaries_transform(data = salaries_df):
+    # Create start_year field from start_date
+    data['start_year'] = data['start_date'].apply(lambda x: int(dt.datetime.fromisoformat(x).strftime('%Y')))
+    
+    # Some annual_base_rates are in hourly amounts
+    # Create annual_salary from annual_base_rates < $250
+    data['annual_salary'] = data['annual_base_rate'].apply(lambda x: x if (x > 250) else x * 40 * 52)
+    
+    # Create employee_status field from employee_classification
+    def employee_status(record):
+        # FT - Full-time
+        if 'FULL-TIME' in record.upper():
+            emp_stat = 'FT'
+        # PT - Part-time
+        elif 'PART-TIME' in record.upper():
+            emp_stat = 'PT'
+        # RET - Retirement
+        elif 'RETIRE' in record.upper():
+            emp_stat = 'RET'
+        # TEMP - Temporary
+        elif 'TEMP' in record.upper():
+            emp_stat = 'TEMP'
+        else:
+            emp_stat = 'OTHER'
+        
+        return emp_stat
+    data['employee_status'] = data['employee_classification'].apply(lambda x: employee_status(x))
+    
+    
+    # Print status
+    print('\nCreating additional salaries features...\n')
+    
+    # Write to /data
+    data.to_csv('data/salaries.csv')
+    
+    return print('\nDone!\n')
+
+# Perform transformations
+census_transform()
+demographics_transform()
+salaries_transform()
